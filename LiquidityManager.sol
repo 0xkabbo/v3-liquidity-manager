bfac43158a745459a585f42aea3d4ceac3c8ad56
Kabbo <161012310+0xkabbo@users.noreply.github.com>
2026-03-24 22:41:08 +0600

    Create LiquidityManager.sol

---
+++ LiquidityManager.sol
@@ -0,0 +1,79 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@uniswap/v3-periphery/contracts/interfaces/INonfungiblePositionManager.sol";
+
+contract LiquidityManager is IERC721Receiver {
+    INonfungiblePositionManager public immutable positionManager;
+
+    struct Deposit {
+        address owner;
+        uint128 liquidity;
+        address token0;
+        address token1;
+    }
+
+    mapping(uint256 => Deposit) public deposits;
+
+    constructor(address _positionManager) {
+        positionManager = INonfungiblePositionManager(_positionManager);
+    }
+
+    function mintNewPosition(
+        address token0,
+        address token1,
+        uint24 fee,
+        int24 tickLower,
+        int24 tickUpper,
+        uint256 amount0Desired,
+        uint256 amount1Desired
+    ) external returns (uint256 tokenId, uint128 liquidity, uint256 amount0, uint256 amount1) {
+        IERC20(token0).transferFrom(msg.sender, address(this), amount0Desired);
+        IERC20(token1).transferFrom(msg.sender, address(this), amount1Desired);
+
+        IERC20(token0).approve(address(positionManager), amount0Desired);
+        IERC20(token1).approve(address(positionManager), amount1Desired);
+
+        INonfungiblePositionManager.MintParams memory params = INonfungiblePositionManager.MintParams({
+            token0: token0,
+            token1: token1,
+            fee: fee,
+            tickLower: tickLower,
+            tickUpper: tickUpper,
+            amount0Desired: amount0Desired,
+            amount1Desired: amount1Desired,
+            amount0Min: 0,
+            amount1Min: 0,
+            recipient: address(this),
+            deadline: block.timestamp
+        });
+
+        (tokenId, liquidity, amount0, amount1) = positionManager.mint(params);
+
+        deposits[tokenId] = Deposit({
+            owner: msg.sender,
+            liquidity: liquidity,
+            token0: token0,
+            token1: token1
+        });
+    }
+
+    function collectAllFees(uint256 tokenId) external returns (uint256 amount0, uint256 amount1) {
+        require(deposits[tokenId].owner == msg.sender, "Not the owner");
+
+        INonfungiblePositionManager.CollectParams memory params = INonfungiblePositionManager.CollectParams({
+            tokenId: tokenId,
+            recipient: msg.sender,
+            amount0Max: type(uint128).max,
+            amount1Max: type(uint128).max
+        });
+
+        (amount0, amount1) = positionManager.collect(params);
+    }
+
+    function onERC721Received(address, address, uint256, bytes calldata) external pure override returns (bytes4) {
+        return this.onERC721Received.selector;
+    }
+}
